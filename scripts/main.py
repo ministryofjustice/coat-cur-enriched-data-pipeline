a896675f437f7d859e9af3dffcbd56202e1fbebe
Lev Gorbunov <lev.gorbunov@justice.gov.uk>
2025-12-09 12:21:53 +0000

    account for s3 path to partitions when creating partitions

--- scripts/main.py
+++ scripts/main.py
@@ -5,6 +5,8 @@ from partitions import filter_billing_periods, create_partition_map
 mode = os.environ.get('MODE')
 
 bucket = "s3://mojap-data-production-coat-cur-reports-v2-hourly-enriched/"
+prefix_to_billing_periods = ""
+path_to_partitions = bucket + prefix_to_billing_periods
 database_name = "cloud_optimisation_and_accountability"
 
 if mode == "dev":
@@ -12,12 +14,12 @@ if mode == "dev":
 elif mode == "prod":
     table_name = "mojap_cur_enriched_data"
 
-columns_types, partitions_types = wr.s3.read_parquet_metadata(path=bucket, dataset=True)
+columns_types, partitions_types = wr.s3.read_parquet_metadata(path=path_to_partitions, dataset=True)
 
 wr.catalog.create_parquet_table(
     database = database_name,
     table = table_name,
-    path = bucket,
+    path = path_to_partitions,
     columns_types = columns_types,
     partitions_types = partitions_types,
     compression = "snappy",
@@ -25,15 +27,15 @@ wr.catalog.create_parquet_table(
     mode= "overwrite"
 )
 
-s3_objects = wr.s3.list_objects(bucket)
+s3_objects = wr.s3.list_objects(path_to_partitions)
 
-billing_period_list = list(set(s3_objects))
+billing_period_list = list(set([object.removeprefix(path_to_partitions).split('/')[0] for object in s3_objects]))
 
 billing_period_list.sort()
 
 filter_billing_periods(billing_period_list)
 
-partitions_values = create_partition_map(billing_period_list)
+partitions_values = create_partition_map(path_to_partitions, billing_period_list)
 
 wr.catalog.add_parquet_partitions(
     database=database_name,
--- scripts/partitions.py
+++ scripts/partitions.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-def filter_billing_periods(billing_periods, date_limit = datetime.now() - relativedelta(years=1)):
-    billing_periods = [
-        bp for bp in billing_periods
+def filter_billing_periods(billing_period_list, date_limit = datetime.now() - relativedelta(years=1)):
+    billing_period_list = [
+        bp for bp in billing_period_list
         if datetime.strptime(bp.split('=')[1], "%Y-%m") >= date_limit
     ]
 
-def create_partition_map(partition_list):
+def create_partition_map(prefix, partition_list):
     partitions_values = {}
 
     for billing_period in partition_list:
-        key = billing_period + "/"
+        key = prefix + billing_period + "/"
         value = [billing_period.split("=")[1]]
         partitions_values[key] = value
 
